9af250db270e35ab68806ac8c8cccc330abedf1e
CarlosBIelemann <carlos.bielemann@estudante.ifms.edu.br>
2021-12-22 14:33:17 -0300

    Tarefa Commit tela de login

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,16 +1,44 @@
 import React, {useState} from 'react';
-import { View, Text , TextInput } from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
+import { View, Text , TextInput, Image, TouchableOpacity} from 'react-native';
 import { styles } from './style';
 
 const Login = () => {
     const [meuTexto,setMeuTexto] = useState('Tere');
+    const navigation = useNavigation<HomePros>();
     return(
-        <View style={styles.container}>
-            <Text>Login</Text>
-            <TextInput style={styles.textInput} 
-                onChangeText={(text)=>{setMeuTexto(text);console.log(meuTexto)}}
-            />
-            <Text>{meuTexto}</Text>
+        <View style={styles.body}>
+            <View style={styles.container}>
+                <View style={styles.topo}>
+                    <Image 
+                        style={styles.Image} 
+                        source={require('../../assets/facebook.png')} 
+                    />
+                    <Text style={styles.titulo}>Facebook</Text>
+                </View>
+                
+
+                <TextInput style={styles.textInput} 
+                    onChangeText={(text)=>{setMeuTexto(text);console.log(meuTexto)}}
+                    placeholder="Email"
+                />
+
+                <TextInput style={styles.textInput} 
+                    onChangeText={(password)=>{setMeuTexto(password);console.log(meuTexto)}}
+                    placeholder="Senha"
+                />
+
+                <View style={styles.viewButton}>
+                    <TouchableOpacity style={styles.button} onPress={navigation=> (navigation.navigate('Home'))}>
+                        <Text style={styles.viewButtonText}>Log in</Text>
+                        
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.link}>
+                    <Text style={styles.linker}>Sign Up for Facebook</Text>
+                </View>
+            </View>
         </View>
     );
 
--- src/Pages/Login/style.ts
+++ src/Pages/Login/style.ts
@@ -1,17 +1,76 @@
 import {StyleSheet} from 'react-native';
+import { withSafeAreaInsets } from 'react-native-safe-area-context';
 
 
 export const styles = StyleSheet.create({
+    body:{
+        backgroundColor:'#4287f5',
+        flex:1,
+        justifyContent:'center',
+    },
     container:{
         flex:1,
         justifyContent:'center',
-        marginHorizontal:100,
+        marginHorizontal:7,
+    },
+    topo:{
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center',
+        marginBottom:50,
+    },
+    titulo:{
+        fontSize:40,
+        fontWeight:'bold',
+        color:'#F5FFFF',
+        marginLeft:7,
+        
     },
     textInput:{
-        height:40,
-        borderColor:"#4369B0",
+        height:45,
+        borderColor:"#F5FFFF",
+        backgroundColor:"#e6f0f0",
         borderWidth:1,
-        borderRadius:0,
+        borderRadius:5,
         color:'black',
+        margin:15,
+        padding:10,
+        fontSize:15,
+    },
+    Image:{
+        height:40,
+        width:40,
+        marginLeft:5,
+    },
+    viewButton:{
+        // backgroundColor:'#3D7BE2',
+        // alignItems:'center',
+        // justifyContent:'center',
+        borderRadius:5,
+        flexDirection:'row',
+        
+    },
+    button:{
+        flex:5,
+        backgroundColor:'#1f4ea6',
+        justifyContent:'center',
+        alignItems:'center',
+        height:45,
+        borderRadius:5,
+        margin:15,
+    },
+    viewButtonText:{
+        color:"#F5FFFF",
+        fontSize:15,
+        fontWeight:'bold',
+    },
+    link:{
+        alignItems:'center',
+        justifyContent:'center',
+        marginTop:65,
+    },
+    linker:{
+        color:'white',
+        textDecorationLine:'underline',
     },
 });
